14a77a77e55bfb041e40ec5c8aa69bd1f7e645ec
Charith Dissanayake <61795948+Charith47@users.noreply.github.com>
2021-01-27 14:44:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,120 @@
+const socket = io('http://localhost:3000');
+const messageContainer = document.getElementById('message-container');
+const messageForm = document.getElementById('send-container');
+const messageInput = document.getElementById('msg-field');
+
+// core functions -----------------------------
+
+function getTime(){
+    // this function handles timing 
+    var date = new Date();
+    var h = date.getHours(); 
+    var m = date.getMinutes();
+
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+
+    var time = h + ":" + m;
+    return time;
+}
+
+function setImage(ID){
+    // this function takes a random string and attaches it to url to set profile image
+    var style = document.createElement('style');
+    style.innerHTML = '#room-picture{background-image:url(https://robohash.org/' + ID + '/?size=65x65);}' 
+    var ref = document.querySelector('script');
+    ref.parentNode.insertBefore(style, ref);
+}
+
+
+function appendMessage(name,message){
+    // this function handles client messages 
+    const messageElement = document.createElement('div');
+    messageElement.classList.add('friend-message');
+    messageElement.innerHTML= "<b>" + name + '</b> <span style="color:rgb(223, 201, 8); font-size:17px;">|</span> ' + message + " " + '<span style="color:rgb(170, 170, 170); font-size:10px;">' + getTime() + "</span>";
+    messageContainer.append(messageElement);
+    
+    //scroll
+    messageContainer.scrollTop = messageContainer.scrollHeight;
+}
+
+function myMessage(message){
+    // this function handles user messages
+    const messageElement = document.createElement('div');
+    messageElement.classList.add('my-message');
+    messageElement.innerHTML = message + " " + '<span style="color:rgb(170, 170, 170); font-size:10px;">' + getTime() + "</span>";
+    messageContainer.append(messageElement);
+
+    //scroll
+    messageContainer.scrollTop = messageContainer.scrollHeight;
+}
+
+function botMessage(message){
+    // this function handles user joined and left messages
+    const messageElement = document.createElement('div');
+    messageElement.classList.add('bot-message');
+    messageElement.innerText = message;
+    messageContainer.append(messageElement);
+
+    //scroll
+    messageContainer.scrollTop = messageContainer.scrollHeight;
+}
+
+// --------------------------------------------
+
+// server handling ----------------------------
+const name = prompt('What is your name?'); // get username
+botMessage('You joined'); // join alert
+socket.emit('new-user', name); // gives the name to the server 
+
+socket.on('hash-code',hash=>{
+    // all clients recieve same hash ID to generate profile image
+    setImage(hash); // inject css to <style> tag
+})
+
+socket.on('group-name',groupName =>{
+    // all clients recieve the same random group name 
+    document.getElementById('room-name').innerHTML = groupName;
+})
+
+socket.on('user-count',count=>{
+    // display user count 
+    if(count > 1) document.getElementById('room-participants').innerHTML = count + " people are online";
+    else document.getElementById('room-participants').innerHTML = "You are all alone, find some friends";
+})
+
+socket.on('chat-message',data =>{
+    // handle incoming chat message
+    appendMessage(`${data.name}`, `${data.message}`);
+}) 
+
+socket.on('user-connected',name =>{
+    // user connected alert
+    botMessage( `${name} joined`);
+})
+
+socket.on('user-disconnected',name =>{
+    // user disconnected alert
+    botMessage( `${name} left`);
+})
+
+messageForm.addEventListener('submit', e=>{
+    // listens to submit and appends client message to message box
+    e.preventDefault(); // prevents page from refreshing on submission
+    const message = messageInput.value; 
+
+    // cannot send empty messages
+    if(message.trim() != ''){
+        myMessage(`${message}`)
+        socket.emit('send-chat-message', message);
+        messageInput.value = '';
+        console.log('submitted');
+    }
+    else{
+        alert("DONT SEND EMPTY MESSAGES YOU MORON!")
+        console.log('empty message');
+    }
+    
+})
+
+// --------------------------------------------
---
+++ server.js
@@ -0,0 +1,50 @@
+// colors 
+const cyan = '\x1b[36m%s\x1b[0m';
+const yellow = '\x1b[33m%s\x1b[0m';
+
+const { uniqueNamesGenerator, adjectives, colors, animals, starWars } = require('unique-names-generator'); 
+
+// server ---------------------------
+const { emit } = require('nodemon');
+const crypto = require('crypto-js');
+const { timers } = require('jquery');
+const io = require('socket.io')(3000);
+const users = {}
+// ----------------------------------
+
+//misc ------------------------------
+var hash = crypto.lib.WordArray.random(16).toString();
+const randomName = uniqueNamesGenerator({ dictionaries: [adjectives, colors, animals] }); // big_red_donkey
+var sentence = randomName.split('_').join(" ");
+var groupName = sentence.charAt(0).toUpperCase() + sentence.slice(1);
+
+var count = 0;
+// ----------------------------------
+
+io.on('connection', socket =>{
+    
+    // log to server and increment user count
+    console.log(cyan,"[SERVER] : A user connected ");  
+    count++;
+    
+    // emit count, robohash, random group name to all users
+    io.emit('user-count',count);
+    io.emit('hash-code',hash.toString());
+    io.emit('group-name',groupName);
+
+    // message handling 
+    socket.on('new-user',name =>{
+        users[socket.id] = name; 
+        socket.broadcast.emit('user-connected',name);
+    })
+    socket.on('send-chat-message',message=>{
+        socket.broadcast.emit('chat-message',{message: message,name: users[socket.id]});
+    })
+    socket.on('disconnect',() =>{
+        console.log(yellow,'[SERVER] : A user disconnected')
+        socket.broadcast.emit('user-disconnected',users[socket.id]);
+        delete users[socket.id];
+        count--;
+        io.emit('user-count',count);
+    })
+}) 
