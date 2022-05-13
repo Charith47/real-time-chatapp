c14582f67096c6d4796fd3b16ebd534e6aaa0d58
Charith Dissanayake <charithdissanayake47@gmail.com>
2022-05-13 12:39:42 +0530

    add cors to socket server. format code

--- server.js
+++ server.js
@@ -1,50 +1,70 @@
-// colors 
+// colors
 const cyan = '\x1b[36m%s\x1b[0m';
 const yellow = '\x1b[33m%s\x1b[0m';
 
-const { uniqueNamesGenerator, adjectives, colors, animals, starWars } = require('unique-names-generator'); 
+const {
+	uniqueNamesGenerator,
+	adjectives,
+	colors,
+	animals,
+} = require('unique-names-generator');
 
 // server ---------------------------
+const server = require('http').createServer();
+
 const { emit } = require('nodemon');
 const crypto = require('crypto-js');
 const { timers } = require('jquery');
-const io = require('socket.io')(3000);
-const users = {}
+const io = require('socket.io')(server, {
+	cors: {
+		origin: '*',
+		methods: ['GET', 'POST'],
+		credentials: false,
+	},
+});
+
+const users = {};
 // ----------------------------------
 
 //misc ------------------------------
 var hash = crypto.lib.WordArray.random(16).toString();
-const randomName = uniqueNamesGenerator({ dictionaries: [adjectives, colors, animals] }); // big_red_donkey
-var sentence = randomName.split('_').join(" ");
+const randomName = uniqueNamesGenerator({
+	dictionaries: [adjectives, colors, animals],
+}); // big_red_donkey
+var sentence = randomName.split('_').join(' ');
 var groupName = sentence.charAt(0).toUpperCase() + sentence.slice(1);
 
 var count = 0;
 // ----------------------------------
 
-io.on('connection', socket =>{
-    
-    // log to server and increment user count
-    console.log(cyan,"[SERVER] : A user connected ");  
-    count++;
-    
-    // emit count, robohash, random group name to all users
-    io.emit('user-count',count);
-    io.emit('hash-code',hash.toString());
-    io.emit('group-name',groupName);
+io.on('connection', (socket) => {
+	// log to server and increment user count
+	console.log(cyan, '[SERVER] : A user connected ');
+	count++;
 
-    // message handling 
-    socket.on('new-user',name =>{
-        users[socket.id] = name; 
-        socket.broadcast.emit('user-connected',name);
-    })
-    socket.on('send-chat-message',message=>{
-        socket.broadcast.emit('chat-message',{message: message,name: users[socket.id]});
-    })
-    socket.on('disconnect',() =>{
-        console.log(yellow,'[SERVER] : A user disconnected')
-        socket.broadcast.emit('user-disconnected',users[socket.id]);
-        delete users[socket.id];
-        count--;
-        io.emit('user-count',count);
-    })
-}) 
+	// emit count, robohash, random group name to all users
+	io.emit('user-count', count);
+	io.emit('hash-code', hash.toString());
+	io.emit('group-name', groupName);
+
+	// message handling
+	socket.on('new-user', (name) => {
+		users[socket.id] = name;
+		socket.broadcast.emit('user-connected', name);
+	});
+	socket.on('send-chat-message', (message) => {
+		socket.broadcast.emit('chat-message', {
+			message: message,
+			name: users[socket.id],
+		});
+	});
+	socket.on('disconnect', () => {
+		console.log(yellow, '[SERVER] : A user disconnected');
+		socket.broadcast.emit('user-disconnected', users[socket.id]);
+		delete users[socket.id];
+		count--;
+		io.emit('user-count', count);
+	});
+});
+
+server.listen(3000);
